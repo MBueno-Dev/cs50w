414333cc051e1516178d8911f9ca889fe58b7626
MBueno-Dev <marcos.mrpb@gmail.com>
2023-09-11 15:39:10 -0300

    refazer wiki

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,10 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("wiki/<str:entry>", views.entry, name="entry"),
-    path("newEntry", views.new, name="new"),
-    path("wiki/<str:entry>/edit", views.edit, name="edit"),
-    path("random", views.random, name="random"),
-    path("search", views.search, name="search")
+    path("", views.index, name="index")
 ]
